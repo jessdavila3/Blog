72c9778f17506813b4d42ec3d4b68e1faf8a6add
Jesse Davila <jessedavila3@gmail.com>
2017-01-10 15:07:52 -0600

    adding security classes, interfaces,dependencies

---
+++ src/main/java/com/codeup/Users.java
@@ -0,0 +1,10 @@
+package com.codeup;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by jessedavila on 1/10/17.
+ */
+public interface Users extends CrudRepository<User, Long> {
+    public User findByUsername(String username);
+}
