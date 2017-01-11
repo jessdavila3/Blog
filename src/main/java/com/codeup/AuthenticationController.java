f915882e8ea4c5647501f6f30b16b9d240928442
Jesse Davila <jessedavila3@gmail.com>
2017-01-11 14:43:31 -0600

    fixed the emai issue with the register page and the User class, users/register now shows and can be submitted

--- src/main/java/com/codeup/AuthenticationController.java
+++ src/main/java/com/codeup/AuthenticationController.java
@@ -2,7 +2,11 @@ package com.codeup;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class AuthenticationController {
@@ -15,5 +19,20 @@ public class AuthenticationController {
     public String showRegisterForm(Model m) {
         m.addAttribute("user", new User());
         return "users/register";
+    }
+
+    @PostMapping("/register")
+    public String createUser(
+            @Valid User user,
+            Errors validation,
+            Model model
+    ) {
+        if (validation.hasErrors()) {
+            model.addAttribute("errors", validation);
+            model.addAttribute("user", user);
+            return "/register";
+        }
+        // something should be happening to create a new user? right?
+        return "redirect:/login";
     }
 }
--- src/main/java/com/codeup/User.java
+++ src/main/java/com/codeup/User.java
@@ -35,4 +35,22 @@ public class User {
     public String getPassword() {
         return password;
     }
+
+    public String getEmail() { return email;}
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
