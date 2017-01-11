f175b2f7adfb9b29c69fc6ff4a51215abe298ac9
Jesse Davila <jessedavila3@gmail.com>
2017-01-11 12:21:50 -0600

    added mapping to register page

--- src/main/java/com/codeup/AuthenticationController.java
+++ src/main/java/com/codeup/AuthenticationController.java
@@ -1,12 +1,19 @@
 package com.codeup;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthenticationController {
     @GetMapping("/login")
     public String showLoginForm() {
-        return "login";
+        return "users/login";
+    }
+
+    @GetMapping("/register")
+    public String showRegisterForm(Model m) {
+        m.addAttribute("user", new User());
+        return "users/register";
     }
 }
