5e4842ea642a2727b873eb63289f4cee4a6484ca
Jesse Davila <jessedavila3@gmail.com>
2017-01-12 09:47:10 -0600

    added navbar div to some pages

--- src/main/java/com/codeup/controllers/UserController.java
+++ src/main/java/com/codeup/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -15,16 +16,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
-    @Autowired
-    public Users usersDao;
+//    @Autowired
+//    public Users usersDao;
 
     @GetMapping("/create")
-    public String createNewUser(Model model) {
+    public String showRegisterPage(Model model) {
         model.addAttribute("user", new User());
         return "users/register";
     }
 
-
+    @PostMapping("/create")
+    public String createUser() {
+        return "posts/index";
+    }
 
 
 }
